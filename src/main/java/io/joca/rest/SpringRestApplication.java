af4a898fc7d08c41eca9d74e62cf33f9f357033a
Joao Berardo <jberardo@me.com>
2019-02-17 17:40:57 -0800

    initializing project.

---
+++ src/main/java/io/joca/rest/SpringRestApplication.java
@@ -0,0 +1,18 @@
+package io.joca.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Joao Berardo
+ * @since Feb. 17, 2019
+ *
+ */
+@SpringBootApplication
+public class SpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApplication.class, args);
+	}
+}
---
+++ src/test/java/io/joca/rest/SpringRestApplicationTests.java
@@ -0,0 +1,22 @@
+package io.joca.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 
+ * @author Joao Berardo
+ * @since Feb. 17, 2019
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
